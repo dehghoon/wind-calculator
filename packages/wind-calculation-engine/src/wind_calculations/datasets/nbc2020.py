8a26bd2d0097b2403124dd2b581f85a3e69c24b7
dehghoon <dehghani.pmp@gmail.com>
2026-08-17 15:06:47 -0400

    feat: complete approved wind calculation engine package

---
+++ packages/wind-calculation-engine/src/wind_calculations/datasets/nbc2020.py
@@ -0,0 +1,6 @@
+"""NBC 2020 rule dataset placeholder.
+
+Only rules explicitly encoded in calculation modules are currently implemented.
+CgCp, Cpi and C&C coefficient tables must be supplied as approved datasets
+before lookup functions are added.
+"""

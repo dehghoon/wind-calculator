c15a99159e175dfd3fa1a77cd3a142ed0f747751
dehghoon <dehghani.pmp@gmail.com>
2026-08-17 15:06:23 -0400

    feat: complete approved wind calculation engine package

---
+++ packages/wind-calculation-engine/src/wind_calculations/validation/__init__.py
@@ -0,0 +1,5 @@
+"""Validation helpers."""
+
+from .numeric import require_between, require_finite_number, require_positive
+
+__all__ = ["require_between", "require_finite_number", "require_positive"]
